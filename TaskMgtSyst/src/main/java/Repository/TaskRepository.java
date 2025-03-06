c266d7baa4f9b6ccc5322d43c77da37fc46e2c71
Herve Sugira <herveonly34@gmail.com>
2025-03-06 12:43:24 +0200

    made some changes to different classes

--- TaskMgtSyst/src/main/java/Repository/TaskRepository.java
+++ TaskMgtSyst/src/main/java/Repository/TaskRepository.java
@@ -1,13 +1,20 @@
 package Repository;
 
 import Entity.Task;
+import Entity.User;
+import Entity.role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.awt.*;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
     Optional<Task> findByTaskName(String taskName);
     Optional<Task> findById(Long taskId);
+    void delete(long id);
+    Optional<User> findByUserName(String userName);
+    List<Task> findByUserAndRole(User user, role role);
 }
--- TaskMgtSyst/src/main/java/Services/TaskServices.java
+++ TaskMgtSyst/src/main/java/Services/TaskServices.java
@@ -2,7 +2,11 @@ package Services;
 
 import Dto.TaskDto;
 import Entity.Task;
+import Entity.User;
+import Entity.role;
+import Repository.RolesRepository;
 import Repository.TaskRepository;
+import Repository.usersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +18,10 @@ public class TaskServices {
 
     @Autowired
     public TaskRepository taskRepository;
+    @Autowired
+    public usersRepository userRepository;
+    @Autowired
+    public RolesRepository rolesRepository;
 
     public Task createTask(TaskDto taskDto) {
         Task task = new Task();
@@ -21,15 +29,29 @@ public class TaskServices {
         return taskRepository.save(task);
     }
 
-    public List<Task> getAllTasks(){
+    public List<Task> getAllTasks() {
         return taskRepository.findAll();
     }
 
-    public Task getTaskById(long id ){
+    public void deleteTask(long id) {
+        taskRepository.delete(id);
+    }
+
+    public Task getTaskById(long id) {
 
         Task task = taskRepository.findById(id)
-                .orElseThrow(()-> new RuntimeException("Task with id: " + id + "not found"));
+                .orElseThrow(() -> new RuntimeException("Task with id: " + id + "not found"));
         return taskRepository.save(task);
+    }
+
+    public List<Task> getTasksByUser(String rolename,String username) {
+        User user = userRepository.findByName(username).
+                orElseThrow(()->new RuntimeException("User with name: " + username + "not found"));
+
+        role role= rolesRepository.findByName(rolename)
+                .orElseThrow(()->new RuntimeException("Role with name: " + rolename + "not found"));
+
+                return taskRepository.findByUserAndRole(user, role);
 
     }
 }
